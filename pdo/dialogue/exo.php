d3146a21339ad9be44cbdbcb188204d71bc616b7
mgandega <mgandega@gmail.com>
2024-08-27 11:09:16 +0200

    dialogue

--- pdo/dialogue/exo.php
+++ pdo/dialogue/exo.php
@@ -4,12 +4,18 @@ $pdo = new PDO("mysql:host=localhost;dbname=ifocop","root","root");
 ?>
 
 <?php
+if(isset($_GET['action']) == 'supprimer'){
+   $resultat = $pdo->exec("DELETE FROM employes WHERE id_employes = '$_GET[idEmploye]' ");
+   if($resultat == 1){
+    echo "un employé supprimé avec succès";
+   }
+}
 $employes = $pdo->query("SELECT * FROM employes");
 
 echo "<table>";
 echo  "<tr><th>prenom</th><th>nom</th><th>sexe</th><th>service</th></tr>";
     while($donnees = $employes->fetch(PDO::FETCH_ASSOC)){
-       echo  "<tr><td>$donnees[prenom]</td><td>$donnees[nom]</td><td>$donnees[sexe]</td><td>$donnees[service]</td></tr>";
+       echo  "<tr><td>$donnees[prenom]</td><td>$donnees[nom]</td><td>$donnees[sexe]</td><td>$donnees[service]</td><td><a href='?action=supprimer&idEmploye=$donnees[id_employes]'>supprimer</a></td></tr>";
     }
 echo "</table>";
 ?>
