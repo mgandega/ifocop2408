c0e0e9701c85d04f90bc8e163b32fc0ea016bf57
mgandega <mgandega@gmail.com>
2024-08-28 15:28:43 +0200

    site ecommerce

--- site/admin/gestion_boutique.php
+++ site/admin/gestion_boutique.php
@@ -3,33 +3,50 @@ include("../inc/init.inc.php");
 include("../inc/haut.inc.php");
 
 // on récupère la photo avec $_FILES
+echo "<a href=''> Ajouter un produit </a><br><br>";
+echo "<a href=''> Afficher un produit </a><br><br>";
+if (!empty($_POST)) {
+    if (!empty($_FILES['photo'])) {
 
+        // debug($_FILES);
+        // time() est appelé timestamp càd le nombre de secondes écoulé depuis le 1er janvier 1970
+        // ce nombre change chaque seconds
+        $unique = time();
+        $reference = 'ifocop_'.time();
+        // debug($unique);
+        // ici on utilise la fonction time() pour avoir un nombre unique, ainsi on évitera d'avoir deux photos de même nom
+        $nom_photo = $unique . '_' . $_FILES['photo']['name'];
+        // $db represente le chemin de la photo à partir du localhost (www ou htdocs)
+        $db = RACINE_SITE . 'photo/' . $nom_photo;
+        // echo $db;
+        // debug($_SERVER['DOCUMENT_ROOT']);
+        // $db represente le chemin de la photo à partir du localhost (www ou htdocs)
 
-if (!empty($_FILES['photo'])) {
+        // $chemin represente le chemin de la photo à partir de la racine du document
+        $chemin = $_SERVER['DOCUMENT_ROOT'] . $db;
+        // echo $chemin;
+        // ici on copie la photo à l'interieur du repertoire photo
+        // le tmp_name represente la photo
+        copy($_FILES['photo']['tmp_name'], $chemin);
+    }
+// debug($_POST);
+$reference = $reference;
+$categorie = $_POST['categorie'];
+$titre = $_POST['titre'];
+$description = $_POST['description'];
+$couleur = $_POST['couleur'];
+$taille = $_POST['taille'];
+$public = $_POST['public'];
+$photo = $db; // /ifocop_2024/site/photo/nom_photo
+$prix = $_POST['prix'];
+$stock = $_POST['stock'];
 
-    debug($_FILES);
-    // time() est appelé timestamp càd le nombre de secondes écoulé depuis le 1er janvier 1070
-    // ce nombre change chaque seconds
-    $unique = time();
-    // debug($unique);
-    // ici on utilise la fonction time() pour avoir un nombre unique, ainsi on évitera d'avoir deux photos de même nom
-    $nom_photo = $unique . '_' . $_FILES['photo']['name'];
-    // $db represente le chemin de la photo à partir du localhost (www ou htdocs)
-    $db = RACINE_SITE . 'photo/' . $nom_photo;
-    // echo $db;
-    // debug($_SERVER['DOCUMENT_ROOT']);
-    // $db represente le chemin de la photo à partir du localhost (www ou htdocs)
+   $resultat = $pdo->exec("INSERT INTO produit(reference,categorie,titre,description,couleur,taille,public,photo,prix,stock) VALUES('$reference','$categorie','$titre','$description','$couleur','$taille','$public','$photo','$prix','$stock')");
+   if($resultat){
+    echo "<span style='background-color:green;color:white'>produit ajouté avec succès<span>";
+   }
 
-    // $chemin represente le chemin de la photo à partir de la racine du document
-    $chemin = $_SERVER['DOCUMENT_ROOT'] . $db;
-    // echo $chemin;
-    // ici on copie la photo à l'interieur du repertoire photo
-    // le tmp_name represente la photo
-    copy($_FILES['photo']['tmp_name'], $chemin);
 }
-// if(!empty($_POST)){
-//     debug($_POST);
-// }
 ?>
 <!-- ne pas oublier  de mettre enctype="multipart/form-data" dans form et de mettre le type en file -->
 <form action="" method="post" enctype="multipart/form-data">
