a4d6a317d1b9f9f9c76e552a9b08e61758e2b544
mgandega <mgandega@gmail.com>
2024-09-04 15:51:32 +0200

    ajax

--- ajax/exos/select.php
+++ ajax/exos/select.php
@@ -1,4 +1,22 @@
 <?php 
-if(isset($_POST['valeur'])){
-    echo $_POST['valeur'];
+$tab = [
+    ['valeur'=>'animaux','selectedCategorie'=>[
+        'tigre','lion','panthère'
+    ]],
+    ['valeur'=>'fruits','selectedCategorie'=>[
+        'poire','pomme','banane'
+    ]],
+    ['valeur'=>'genre','selectedCategorie'=>[
+        'homme','femme','mixte'
+    ]],
+];
+if(isset($_GET['valeur'])){
+    foreach($tab as $val){
+        if($val['valeur'] == $_GET['valeur']){
+            foreach($val['selectedCategorie'] as $cat){
+                echo "<option value='$cat'>$cat</option>";
+            } 
+        }
+
+    }
 }
