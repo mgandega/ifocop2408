ca68c044549cf7c7f3de4016825b8e90199c2b68
mgandega <mgandega@gmail.com>
2024-09-04 12:27:04 +0200

    ajax

---
+++ ajax/affichage_POST/affichage.php
@@ -0,0 +1,9 @@
+<?php
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+// on peut aussi afficher le tableau tel qu'il est et le parser et l'afficher sur la page affichage.html
+// $json = json_encode($_POST);
+
+echo "hello $_POST[prenom] $_POST[nom] " ;
+
---
+++ ajax/exos/json_exo.php
@@ -0,0 +1,5 @@
+<?php
+$personnes = ["prenom"=>"jean","ville"=>"paris","ecole"=>"ifocop"];
+
+$json = json_encode($personnes);
+echo $json;
